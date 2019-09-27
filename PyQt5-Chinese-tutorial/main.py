fdf37647480bf095bae141de347558027bdd66a6
ie456 <s5xbrian@gmail.com>
2019-09-27 17:41:39 +0800

    20190927 learning update

--- PyQt5-Chinese-tutorial/main.py
+++ PyQt5-Chinese-tutorial/main.py
@@ -1,15 +1,51 @@
 
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow,QFileDialog
+from PyQt5.QtWidgets import (QApplication,QMainWindow,QFileDialog,qApp,QMessageBox)
+from PyQt5 import QtGui
 from MainPage import *
+
 
 class MyMainWindow (QMainWindow,Ui_MainWindow):
     def __init__(self,parent=None):
         super(MyMainWindow,self).__init__(parent)
         self.setupUi(self)
+        self.action_Exit.triggered.connect(qApp.quit)
+        self.actionOpen_File.triggered.connect(self.openfile_New)
+        self.actionImportFile.triggered.connect(self.test)
+        self.isChanged = 0
 
+    def file_open(self):
+        name = QFileDialog.getOpenFileName(self, 'Open File')
+        file = open(name, 'r')
 
+        self.editor()
+        with file:
+            text = file.read()
+            self.textEdit.setText(text)
 
+    def editor(self):
+        self.textEdit = QtGui.QTextEdit()
+        self.setCentralWidget(self.textEdit)
+
+    def openfile_New(self):
+        if self.isChanged == True:
+            quit_msg = "<b>The Document was changed.<br>Do you want to save changes?</ b>"
+            reply = QMessageBox.question(self, 'Save Confirmation',quit_msg, QMessageBox.Yes, QMessageBox.No)
+            if reply == QMessageBox.Yes:
+                self.saveOnQuit()
+        fileName,_ = QFileDialog.getOpenFileName(self, "Select file", "./",
+                                                   "Bom file(*.txt *.csv *.xlsx *.xlsm *.xls);; \
+                                                   Text Files (*.txt);; \
+                                    Excel file (*.csv *.xlsx *.xlsm *.xls)")
+        if fileName:
+            #self.loadCsvOnOpen(fileName)
+            QMessageBox.warning(self,'','Get!'+ fileName)
+
+    def test(self):
+        reply = QMessageBox.information(self,  # 使用infomation信息框
+                                        "标题",
+                                        "消息",
+                                        QMessageBox.Yes | QMessageBox.No)
 
 
 
--- PyQt5-Chinese-tutorial/status_7.py
+++ PyQt5-Chinese-tutorial/status_7.py
@@ -1,8 +1,38 @@
-# -*- coding: utf-8 -*-
+import sys
+from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication
+from PyQt5.QtGui import QIcon
 
-# Form implementation generated from reading ui file 'status_7.py'
-#
-# Created by: PyQt5 UI code generator 5.11.2
-#
-# WARNING! All changes made in this file will be lost!
 
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        textEdit = QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        exitAct = QAction(QIcon('exit24.png'), 'Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(self.close)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAct)
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exitAct)
+
+        self.setGeometry(300, 300, 350, 250)
+        self.setWindowTitle('Main window')
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
